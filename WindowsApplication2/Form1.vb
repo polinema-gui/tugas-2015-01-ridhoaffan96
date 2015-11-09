4cf5f73f5f08bc53c7e55c4f98537f011c64c07b
ridhoaffan96 <ridhoaffan96@gmail.com>
2015-11-09 22:08:44 +0700

    Update

---
+++ WindowsApplication2/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Me.BackColor = Color.Blue Then
+            Me.BackColor = Color.Red
+        Else
+            Me.BackColor = Color.Blue
+        End If
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.BackColor = Color.Blue
+    End Sub
+End Class
